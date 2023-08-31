9649bfeba20154b4b06c2933a6c7aa6edab1d80c
thirae <132889027+thirae@users.noreply.github.com>
2023-08-31 17:48:30 +0900

    8/31 ファイル保存完成、読み込み処理途中、登録画面完成

--- hiraeTodo/TodoApp/TodoApp/View/MainWindow.xaml.cs
+++ hiraeTodo/TodoApp/TodoApp/View/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace TodoApp
 {
@@ -10,24 +15,125 @@ namespace TodoApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ObservableCollection<Task> tasks = new ObservableCollection<Task>();
+        public ObservableCollection<Task> tasks;
+
+        // ファイルパス
+        private string csvPath = "save.csv";
 
         public MainWindow()
         {
             InitializeComponent();
 
+            tasks = ReadCsvFile(csvPath);
             TaskDataGrid.ItemsSource = tasks;
-
-            // MVVM用
-            // DataContext = new MainWindowVM();
         }
 
+        // CsvFile読み込み
+        public ObservableCollection<Task> ReadCsvFile(string filePath)
+        {
+            var collection = new ObservableCollection<Task>();
+
+            try
+            {
+                // 引数のパスにファイルが存在するか
+                if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException("ファイルが見つかりません");
+                }
+
+                // 行データをカンマ区切りで分割するようにファイルオープン
+                using (var parser = new TextFieldParser(filePath))
+                {
+                    parser.Delimiters = new string[] { "," };
+                    // 最終行まで繰り返す
+                    while (!parser.EndOfData)
+                    {
+                        // 文字列中の前後スペースを削除しない
+                        parser.TrimWhiteSpace = false;
+
+                        // カンマで分割した文字列の配列を取得
+                        var cells = parser.ReadFields();
+
+                        // 登録
+                        collection.Add(new Task(cells[1], cells[2]));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // エラー内容を出力
+                Debug.WriteLine(ex.Message);
+            }
+            return collection;
+        }
+
+        /// <summary>
+        /// ファイル書き込みメソッド
+        /// </summary>
+        public void FileSave()
+        {
+            // 書き込み処理
+            using (var sw = new StreamWriter(csvPath,false, Encoding.Unicode))
+            {
+                // 列ヘッダ
+                sw.WriteLine(string.Join(",",TaskDataGrid.Columns
+                    .Select(x =>
+                    {
+                        if (x.Header is string headerText)
+                        {
+                            return headerText;
+                        }
+                        //else if (TaskDataGrid)
+                        //{
+                        //
+                        //}
+                        else
+                        {
+                            return string.Empty;
+                        }
+                    })));
+
+                // セルの内容
+                foreach (var item in TaskDataGrid.Items)
+                {
+                    sw.WriteLine(string.Join(",",TaskDataGrid.Columns
+                        .Select(x => x.OnCopyingCellClipboardContent(item)?.ToString())));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var tag = ((Button)sender).Tag as Task;
+            tasks.Remove(tag);
+            FileSave();
+        }
+
+        /// <summary>
+        /// 登録ボタンを押した際のイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void RegButtonClick(object sender, RoutedEventArgs e)
         {
-            tasks.Add(new Task("sssssssssss", "sasa"));
-            TaskDataGrid.ItemsSource = tasks;
-            // RegisterWindow registerWindow = new RegisterWindow(this);
-            // registerWindow.ShowDialog();
+            // 登録画面に遷移
+            RegisterWindow registerWindow = new RegisterWindow(this);
+            registerWindow.ShowDialog();
+        }
+
+        /// <summary>
+        /// セルが更新したら起きるイベント(フォーカスを失ったら)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TaskDataGrid_SourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
+        {
+            FileSave();
         }
     }
 }
--- hiraeTodo/TodoApp/TodoApp/View/RegisterWindow.xaml.cs
+++ hiraeTodo/TodoApp/TodoApp/View/RegisterWindow.xaml.cs
@@ -1,35 +1,31 @@
-﻿//using System.Collections.Generic;
-//using System.Collections.ObjectModel;
-//using System.Windows;
-//using System;
-//
-//namespace TodoApp
-//{
-//    /// <summary>
-//    /// Register.xaml の相互作用ロジック
-//    /// </summary>
-//    public partial class RegisterWindow : Window
-//    {
-//        private MainWindow _mainWindow = null;
-//        private ObservableCollection<Task> tasks = new ObservableCollection<Task>();
-//        public RegisterWindow(MainWindow mainWindow)
-//        {
-//            _mainWindow = mainWindow;
-//
-//            InitializeComponent();
-//
-//            // MVVM
-//            // DataContext = new RegisterWindowVM();
-//        }
-//
-//        //MainWindowへのデータの受け渡し
-//        private void RegButtonClick(object sender, RoutedEventArgs e)
-//        {
-//            tasks.Add(new Task(DeadlineText.Text, TaskText.Text));
-//            _mainWindow.TaskDataGrid.ItemsSource = new ObservableCollection<Task>();
-//            //_mainWindow.TaskDataGrid.ItemsSource = tasks;
-//            Close();
-//        }
-//
-//    }
-//}
+﻿using System.Windows;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Register.xaml の相互作用ロジック
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        private MainWindow _mainWindow = null;
+        public RegisterWindow(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+
+            InitializeComponent();
+        }
+
+        //MainWindowへのデータの受け渡し
+        private void RegButtonClick(object sender, RoutedEventArgs e)
+        {
+            // データグリッド書き込み
+            _mainWindow.tasks.Add(new Task(DeadlineText.Text, TaskText.Text));
+            _mainWindow.TaskDataGrid.ItemsSource = _mainWindow.tasks;
+
+            _mainWindow.FileSave();
+
+            // Windowを閉じる
+            Close();
+        }
+    }
+}
